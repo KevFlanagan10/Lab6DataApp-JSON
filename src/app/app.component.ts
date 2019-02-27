a7774174dd965b00bfab02add5e1a384b856e161
KevFlanagan <G00228079@gmit.ie>
2019-02-27 15:46:30 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {DataService} from './services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  
 })
-export class AppComponent {
-  title = 'Data-App';
+export class AppComponent implements OnInit {
+  students:any = [];
+  constructor(private dataService:DataService){
+
+
+  }
+  ngOnInit(){
+    this.dataService.GetStudentData().subscribe((data)=>{
+    this.students = data.students;
+    });
+
+  }
+    
 }
